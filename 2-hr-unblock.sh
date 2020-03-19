545674fb52f59e101bd2aeae98326abd058ff750
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-03-19 14:41:53 +0100

    generate /dev/shm/hr.ip.list.unblock

--- 2-hr-unblock.sh
+++ 2-hr-unblock.sh
@@ -4,3 +4,5 @@ grep -v ';;;' /dev/shm/fail2ban /dev/shm/public_blacklist | \
 	awk '{ print $3 " " $2 }' | sort -u | grep -v LIST | tee /dev/shm/ip.list | awk '{ print $1 }' | \
 	xargs -i sh -c 'geoiplookup {} | sed "s/^/{} /"' | grep HR | tee /dev/shm/geo.hr | \
 	awk '{ print $1 }' > /dev/shm/hr.ips.unblock
+
+grep -f /dev/shm/hr.ips.unblock /dev/shm/ip.list > /dev/shm/hr.ip.list.unblock
