21838df9ed8ab84d7ea573bcd902bf1bcd907bf8
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-03-25 09:24:50 +0100

    rename and shabang

--- 11-get-fail2ban.sh
+++ 11-get-fail2ban.sh
@@ -1,1 +1,3 @@
+#!/bin/sh -xe
+
 ssh -i /home/dpavlin/.ssh/mtik/enesej enesej@193.198.212.1 "/ip firewall address-list print where list=fail2ban" | tee /dev/shm/fail2ban
--- 12-get-public_blacklist.sh
+++ 12-get-public_blacklist.sh
@@ -1,1 +1,3 @@
+#!/bin/sh -xe
+
 ssh -i /home/dpavlin/.ssh/mtik/enesej enesej@193.198.212.1 "/ip firewall address-list print where list=public_blacklist" | tee /dev/shm/public_blacklist
